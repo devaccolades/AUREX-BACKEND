2a3f532bef6f2e9f81fa0d0a80d015c0a35aa9a6
aswin <aswink.accolades@gmail.com>
2026-01-20 12:05:52 +0530

    faq section api

--- general/urls.py
+++ general/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path('blogs/', BlogListAPIView.as_view(), name='blog-list'),
     path("blogs/<str:slug>/", BlogRetrieveAPIView.as_view(), name="blog-detail"),
     path('faq/', FaqListAPIView.as_view(), name='blog-list'),
-    path("faq/<str:slug>/", FaqRetrieveAPIView.as_view(), name="blog-detail"),
     path('testimonials/', TestimAPIView.as_view(), name='testim-list'),
     path('v-testimonials/', VidTestimAPIView.as_view(), name='vid-testim-list'),
     path('aboutus/', AboutAPIView.as_view(), name='aboutus'),
