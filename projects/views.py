90723bdd6bc93c2150218a344e387eae5a313a76
manjimanm <manjimanm001@gmail.com>
2026-03-26 10:37:55 +0530

    projects static content  for seo api creation

--- projects/views.py
+++ projects/views.py
@@ -210,3 +210,24 @@ class ProjectUpdatesView(APIView):
 
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class ProjectsPageTitlesAndTextView(APIView):
+    serializer_class = projects_serializer.ProjectsPageTitlesAndTextSerializer
+
+    def get(self, request, slug=None):
+        try:
+            if slug:
+                project = projects_models.Projects.objects.filter(slug=slug, is_deleted=False).first()
+                if not project:
+                    return Response({"error": "Project not found"}, status=status.HTTP_404_NOT_FOUND)
+
+                data = projects_models.ProjectsPageTitlesAndText.objects.filter(project=project, is_deleted=False)
+            else:
+                data = projects_models.ProjectsPageTitlesAndText.objects.filter(is_deleted=False)
+
+            serializer = self.serializer_class(data, many=True, context={"request": request})
+            return Response(serializer.data)
+
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
